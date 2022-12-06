5177e2e9901265a780dd909e2f4edb7106446b56
zahiahmed <zahi.ahmed33@gmail.com>
2022-12-06 17:10:14 -0500

    zahi nav emmett drive phase4

--- skeleton/lib/03a_nobel_bonus.rb
+++ skeleton/lib/03a_nobel_bonus.rb
@@ -11,5 +11,15 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+  SELECT
+    DISTINCT(physics_years.yr)
+  FROM 
+    nobels AS physics_years
+  JOIN
+    nobels AS chemistry_years
+  ON
+    physics_years.subject = chemistry_years.subject
+  WHERE
+    physics_years.subject = 'Physics';
   SQL
 end
